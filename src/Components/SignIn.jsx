bb5961e5a8e030ab84de9f8739327ab9c6e397ae
Trung Pham <phamtrung2422k@gmail.com>
2020-12-21 20:34:33 +0700

    add axios

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,56 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateAccount from "../blocks/CreateAccount";
 import { Input, Form, Button } from "antd";
+import axios from "axios";
 
-class SignInComponent extends React.Component {
-  render() {
-    return (
-      <CreateAccount>
-        <div className="wrapper">
-          <div className="logo">
-            <img src="/images/logo.png" alt="logo" />
-            <br />
-            <span className="create-account-text">Welcome to SMCT</span>
-          </div>
-          <Form
-            name="normalSignIn"
-            className="create-account"
-            initialValues={{ remember: true }}
-            // onFinish={onFinish}
-          >
-            <Form.Item
-              name="email"
-              rules={[{ required: true, message: "Please input your Email!" }]}
-            >
-              <Input placeholder="Email" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-              ]}
-            >
-              <Input placeholder="Password" />
-            </Form.Item>
-          </Form>
-          <Button
-            className="btn-sign-up"
-            type="primary"
-            block
-            // icon={<UserAddOutlined />}
-          >
-            Sign In
-          </Button>
+const SignInComponent = () => {
+  const [form] = Form.useForm();
+  const [name, setName] = useState("");
+
+  const createAccount = async () => {
+    try {
+      await form.validateFields();
+    } catch (error) {}
+  };
+
+  return (
+    <CreateAccount>
+      <div className="wrapper">
+        <div className="logo">
+          <img src="/images/logo.png" alt="logo" />
           <br />
-          <a href="https://github.com/truph77">forget your password</a>
-          <div className="tail">
-            Not our member yet?
-            <a href="./create"> Create Account!</a>
-          </div>
+          <span className="create-account-text">Welcome to SMCT</span>
         </div>
-      </CreateAccount>
-    );
-  }
-}
+
+        <Form
+          form={form}
+          name="normalSignIn"
+          className="create-account"
+          initialValues={{ remember: true }}
+          // onFinish={onFinish}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your Email!" }]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input placeholder="Password" />
+          </Form.Item>
+        </Form>
+
+        <Button
+          className="btn-sign-up"
+          type="primary"
+          block
+          // icon={<UserAddOutlined />}
+        >
+          Sign In
+        </Button>
+
+        <br />
+        <a href="https://github.com/truph77">forget your password</a>
+        <div className="tail">
+          Not our member yet?
+          <a href="./create"> Create Account!</a>
+        </div>
+      </div>
+    </CreateAccount>
+  );
+};
 
 export default SignInComponent;
